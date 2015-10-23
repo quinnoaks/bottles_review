90718a3941fc3d413cdb3123fbee3bd5448004b0
Quinn Oaks <quinn.oaks@gmail.com>
2015-10-22 20:14:39 -0700

    Add last test.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,7 @@
 var beerWall = function(number) {
   if ((number >= 1 && number <= 99) || (number === 0)) {
     return true;
+  } else {
+    return false;
   }
 };
--- spec/specs.js
+++ spec/specs.js
@@ -6,4 +6,12 @@ describe('beerWall', function() {
   it("is true for number 0", function() {
     expect(beerWall(0)).to.equal(true);
   });
+
+  it("is false for numbers under 0", function() {
+    expect(beerWall(-4)).to.equal(false);
+  });
+
+  it("is false for numbers above 99", function() {
+    expect(beerWall(115)).to.equal(false);
+  });
 });
